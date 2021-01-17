9f46bb83338710cf1fa091ca7befd8efba908f6c
Julif <julif-@live.com>
2021-01-17 20:03:07 -0300

    v 0.1.1

--- app_info.js
+++ app_info.js
@@ -1,4 +1,4 @@
 const app_name = " Deckbuilder ";
 const app_web = "julif.github.io/red ";
 const github_rep = "https://github.com/julif/red";
-const app_version = " v 0.1 ";
+const app_version = " v 0.1.1 ";
